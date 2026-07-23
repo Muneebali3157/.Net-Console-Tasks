e72ce52350a8c956e44dd6c1ba7a2f841a9baa52
Muneeb Ali <mianmuneeb3157@gmail.com>
2026-07-23 21:10:03 +0500

    Add files via upload

---
+++ Task 2 — Number Properties Toolkit/Program.cs
@@ -0,0 +1,107 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1 {
+    class Program { 
+
+   public static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter a number : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n\n");
+
+            isprime(x);
+
+
+            ispalindrom(x);
+
+
+            isarmstrong(x);
+
+
+            PrintFactorial(x);
+
+
+            static void isprime(int x)
+            {
+
+                if (x < 0)
+                {
+                    Console.WriteLine(x + " is not a prime number.");
+                }
+                for (int i = 2; i < x / 2; i++)
+                {
+                    if (x % i == 0)
+                    {
+
+                        Console.WriteLine($"{x} is not a prime number.");
+                        return;
+
+                    }
+                }
+
+                Console.WriteLine($"{x} is a prime number.");
+
+            }
+            static void ispalindrom(int x)
+            {
+                if (x < 0)
+                {
+                    Console.WriteLine($"{x} is not a palindrome number.");
+                }
+                int original = x;
+                int reverse = 0;
+
+                while (original > 0)
+                {
+                    int digit = original % 10;
+                    reverse = (reverse * 10) + digit;
+                    original = original / 10;
+                }
+                Console.WriteLine("Reverse value is : " + reverse);
+
+            }
+            static void isarmstrong(int x)
+            {
+                if (x < 0)
+                {
+                    Console.WriteLine("number is less than 0 ");
+                }
+                int temp = x;
+                int sum = 0;
+                while (temp > 0)
+                {
+                    {
+                        int digit = temp % 10;
+                        sum += digit * digit * digit;
+                        temp = temp / 10;
+
+                    }
+                }
+                Console.WriteLine("after taking cube of each value sum of the values is : " + sum);
+            }
+
+            static void PrintFactorial(int x)
+            {
+                if (x > 0)
+                {
+                    long ans = 1;
+                    for (int i = 1; i <= x; i++)
+                    {
+
+                        ans *= i;
+
+                    }
+                    Console.WriteLine($"Factorial of number {x} is : " + ans);
+                }
+
+            }
+        }
+    } }
---
+++ Task 3 — Student Class (OOP Fundamentals)/Program.cs
@@ -0,0 +1,109 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1
+{
+    public class Student
+    {
+        public int Roll_no { get; private set; }
+        public string Name { get; private set; }
+        public int[] Marks { get; private set; }
+
+        public Student(int roll_no, string name, int[] marks)
+        {
+            Roll_no = roll_no;
+            Name = name;
+            Marks = marks;
+        }
+        public int CalculateTotal()
+        {
+            return Marks.Sum();
+        }
+        public int CalculatePercentage()
+        {
+            return CalculateTotal() / Marks.Length;
+
+        }
+        public string GetGrade()
+        {
+            if (CalculatePercentage() >= 90)
+            {
+                return "A";
+            }
+            else if (CalculatePercentage() >= 80)
+            {
+                return "B";
+            }
+            else if (CalculatePercentage() >= 70)
+            {
+                return "C";
+            }
+            else if (CalculatePercentage() >= 60)
+            {
+                return "D";
+            }
+            else
+            {
+                return "F";
+            }
+
+        }
+
+
+
+        public static void displayStudents()
+        {
+            List<Student> students = new List<Student>() {
+
+            new Student(1, "Alice", new int[] { 85, 90, 78, 92, 88 }),
+            new Student(2, "Bob", new int[] { 75, 80, 70, 85, 90 }),
+            new Student(3, "Ali", new int[] { 95, 92, 88, 90, 94 }),
+            new Student(4, "David", new int[] { 60, 65, 70, 75, 80 }),
+            new Student(5, "Eve", new int[] { 50, 55, 60, 65, 70 })
+            };
+            Console.WriteLine("\t Roll No \t Name \t\t Total \t\t Percentage \t Grade \t\t Marks ");
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"\t{student.Roll_no} \t\t {student.Name} \t\t {student.CalculateTotal()} \t\t {student.CalculatePercentage()} \t\t {student.GetGrade()} \t\t {string.Join(", ", student.Marks)}");
+            }
+
+
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Student.displayStudents();
+            }
+
+        }
+
+
+        //class Program
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        Console.WriteLine("Enter Roll Number: ");
+        //        int roll_no = Convert.ToInt32(Console.ReadLine());
+        //        Console.WriteLine("Enter Name: ");
+        //        string name = Console.ReadLine();
+        //        int[] marks = new int[5];
+        //        for (int i = 0; i < 5; i++)
+        //        {
+        //            Console.WriteLine($"Enter Marks for Subject {i + 1}: ");
+        //            marks[i] = Convert.ToInt32(Console.ReadLine());
+        //        }
+        //        Student student = new Student(roll_no, name, marks);
+        //        Console.WriteLine($"\nStudent Details:\nRoll Number: {student.Roll_no}\nName: {student.Name}\nTotal Marks: {student.CalculateTotal()}\nPercentage: {student.CalculatePercentage()}%\nGrade: {student.GetGrade()}");
+        //    }
+        //}
+
+    }
+
+}
